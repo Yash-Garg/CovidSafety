8d7ffeeb1006382f8dff31725edcaebda984bf36
Yash-Garg <ben10.yashgarg@gmail.com>
2020-07-10 19:29:38 +0530

    Add tips page [WIP]

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+(function($) {
+    "use strict";
+
+    $(window).on('scroll load', function() {
+        if ($(".navbar").offset().top > 20) {
+            $(".fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    $(function() {
+        $(document).on('click', 'a.page-scroll', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 600, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    $(".navbar-nav li a").on("click", function(event) {
+        if (!$(this).parent().hasClass('dropdown'))
+            $(".navbar-collapse").collapse('hide');
+    });
+})(jQuery);
